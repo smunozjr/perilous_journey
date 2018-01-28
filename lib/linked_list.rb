0f9227f822216e4e2113f870d40e6f05351f32d0
smunozjr1986 <sergiomunozjr07@gmail.com>
2018-01-28 15:53:10 -0700

    complete iteration one

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,14 +1,24 @@
 require './lib/node'
 
 class LinkedList
-  attr_reader :head
+  attr_reader :head,
+              :count
 
   def initialize
     @head = nil
+    @count = 0
   end
 
   def append(name)
+    @node = Node.new(name)
+    @head = @node if head.nil?
+    @count += 1
+    @node
+  end
 
+  def to_string
+    # require "pry"; binding.pry
+    "The #{@node.surname} family"
   end
 
 
--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -1,4 +1,5 @@
 require './lib/linked_list'
+require './lib/node'
 require 'minitest/autorun'
 require 'minitest/pride'
 
@@ -13,14 +14,26 @@ class LinkedListTest <Minitest::Test
   def test_list_head_is_nil
     list = LinkedList.new
 
-    assert_equal nil, list.head
+    assert_nil nil, list.head
   end
 
   def test_append_new_name
     list = LinkedList.new
 
-    assert_equal "West", list.append("West")
+    assert_instance_of Node, list.append("West")
+    assert_nil nil, list.head.next_node
+    assert_equal 1, list.count
   end
+
+  def test_list_to_string
+    list = LinkedList.new
+    list.append("West")
+
+    assert_equal "The West family", list.to_string
+  end
+
+
+
 
 
 
