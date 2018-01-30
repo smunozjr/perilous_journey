db1a7d973f7c89f34d0f0f9cdcc33e4a84621f8d
smunozjr1986 <sergiomunozjr07@gmail.com>
2018-01-30 14:53:08 -0700

    working on append method making recursive

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -11,11 +11,22 @@ class LinkedList
   end
 
   def append(name)
-    @head = Node.new(name)
-    @head = @node if head.nil?
+    if @head.nil?
+      @head = Node.new(name)
+    else
+      new_node_position = traverse_list
+      new_node_position = Node.new(name)
+    end
     @count += 1
-    @names_list << name
-    @node
+  end
+
+  def traverse_list(current_node = @head)
+    if current_node.next_node = nil
+       return current_node.next_node
+    else
+      current_node = current_node.next_node
+       traverse_list(current_node)
+     end
   end
 
   def to_string
